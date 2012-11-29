00afca3d04951297608d4a1e9fc2e0d5fa9d4d14
Your Name <jackiegleason@gmail.com>
2012-11-29 13:58:57 -0500

    Formatting stuff

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/aboutMe', routes.aboutMe);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,6 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+exports.aboutMe = function(req, res){
+  res.render('aboutMe', { title: 'Express' });
+};
